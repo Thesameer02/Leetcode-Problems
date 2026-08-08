6c273786c55a0837e086aa9c9914ca2023ea197b
Thesameer02 <sj368592@gmail.com>
2026-08-09 03:40:30 +0530

    CodeSync: Add solution for 189. Rotate Array

---
+++ 0189-rotate-array/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void reverse(int i,  int j, vector<int>& nums){
+        while(i<=j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n= nums.size();
+        k=k%n;
+        reverse(0,n-1,nums);
+        reverse(0,k-1,nums);
+        reverse(k,n-1,nums);
+    }
+};
